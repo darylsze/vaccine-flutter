783a63dbeb936fe2e8349963d98559d2bda89e6f
darylsze <sze.kf1004@gmail.com>
2021-04-04 12:40:25 +0800

    feat: able to receive push while navigation bar works

--- lib/presentation/home/view.dart
+++ lib/presentation/home/view.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:vaccine_hk/presentation/MessageHandler.dart';
+import 'package:vaccine_hk/presentation/articleDetails/article_details_page.dart';
 import 'package:vaccine_hk/presentation/articles/article_list_page.dart';
 import 'package:vaccine_hk/presentation/centerList/page.dart';
 import 'package:vaccine_hk/widgets/index.dart';
@@ -14,6 +16,25 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   int _selectedIndex = 0;
 
+  @override
+  void initState() {
+    super.initState();
+
+    FirebaseMessaging.onMessageOpenedApp.listen((message) {
+      print("on push click to open app: $message");
+      if (message.data.containsKey("id") &&
+          message.data.containsKey("path") &&
+          (message.data['path'] as String).contains("/articles/")) {
+        Navigator.pushNamed(
+          context,
+          ArticleDetailsPage.routeName,
+          arguments: ArticleDetailsPageArguments(message.data['id'] as String),
+        );
+      } else {
+        print("either id or path not satifished, skip navigation");
+      }
+    });
+  }
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
@@ -22,31 +43,29 @@ class _HomePageState extends State<HomePage> {
 
   // fixme ordering
   final List<Widget> _widgetOptions = <Widget>[
-    ArticleListPage(),
     CenterList(),
+    ArticleListPage(),
   ];
 
   @override
   Widget build(BuildContext context) {
-    return MessageHandler(
-      child: Scaffold(
-        drawer: MyDrawer(),
-        body: _widgetOptions.elementAt(_selectedIndex),
-        bottomNavigationBar: BottomNavigationBar(
-          backgroundColor: Colors.white,
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: '首頁',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.article),
-              label: '最新消息',
-            ),
-          ],
-          currentIndex: _selectedIndex,
-          onTap: _onItemTapped,
-        ),
+    return Scaffold(
+      drawer: MyDrawer(),
+      body: _widgetOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '首頁',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.article),
+            label: '最新消息',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
       ),
     );
   }
